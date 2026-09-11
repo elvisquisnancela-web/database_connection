55df749a05a4263a7622daeee3ae9d7331f2aef5
elvisquisnancela-web <elvisquisnancela@gmail.com>
2026-09-11 09:17:03 +0200

    Rename dbtest to health and update route

--- app.py
+++ app.py
@@ -1,43 +1,14 @@
 import azure.functions as func
-import psycopg2
-import os
 
 app = func.FunctionApp()
 
 @app.route(
-    route="dbtest",
+    route="health",
     auth_level=func.AuthLevel.ANONYMOUS
 )
-def dbtest(req: func.HttpRequest):
+def health(req: func.HttpRequest):
 
-    try:
-
-        conn = psycopg2.connect(
-            host="gistest.postgres.database.azure.com",
-            database="postgres",
-            user="adminelvis",
-            password="Evsleo333",
-            sslmode="require"
-        )
-
-        cur = conn.cursor()
-
-        cur.execute("SELECT current_database();")
-
-        db = cur.fetchone()[0]
-
-        cur.close()
-        conn.close()
-
-        return func.HttpResponse(
-            f"Connected to {db}",
-            status_code=200
-        )
-
-    except Exception as e:
-
-        return func.HttpResponse(
-            str(e),
-            status_code=500
-        )
-
+    return func.HttpResponse(
+        "OK",
+        status_code=200
+    )
