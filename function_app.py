fb162dd4a1c5575a1419695c173ab79981b69f44
elvisquisnancela-web <elvisquisnancela@gmail.com>
2026-09-11 11:38:35 +0200

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -4,22 +4,14 @@ import importlib.util
 app = func.FunctionApp()
 
 @app.route(
-    route="health",
-    auth_level=func.AuthLevel.ANONYMOUS
-)
-def health(req: func.HttpRequest):
-    return func.HttpResponse("OK")
-
-
-@app.route(
     route="packages",
     auth_level=func.AuthLevel.ANONYMOUS
 )
 def packages(req: func.HttpRequest):
 
-    result = {
-        "psycopg2": importlib.util.find_spec("psycopg2") is not None,
-        "psycopg": importlib.util.find_spec("psycopg") is not None
-    }
-
-    return func.HttpResponse(str(result))
+    return func.HttpResponse(
+        str({
+            "psycopg2": importlib.util.find_spec("psycopg2") is not None,
+            "psycopg": importlib.util.find_spec("psycopg") is not None
+        })
+    )
