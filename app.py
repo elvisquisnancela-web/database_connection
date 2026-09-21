b87284f0470db90025fe398634c33ea28b30de50
elvisquisnancela-web <elvisquisnancela@gmail.com>
2026-09-21 14:47:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,39 +7,10 @@ import os
 app = Flask(__name__)
 
 
-@app.route("/health")
-def health():
+@app.route("/hello")
+def hello():
+    return "<h1>Hello, my friend!</h1>"
 
-    try:
-
-        conn = psycopg2.connect(
-            host="gistest.postgres.database.azure.com",
-            port="5432",
-            database="postgres",
-            user="adminelvis",
-            password="Evsleo333"
-        )
-
-        cur = conn.cursor()
-
-        cur.execute("SELECT version();")
-
-        version = cur.fetchone()[0]
-
-        conn.close()
-
-        return jsonify({
-            "status": "healthy",
-            "database": "connected",
-            "version": version
-        })
-
-    except Exception as ex:
-
-        return jsonify({
-            "status": "error",
-            "message": str(ex)
-        }), 500
 
 
 @app.route("/query")
